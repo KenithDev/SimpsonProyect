48b3eb73b4999c78b055a06e932f0f9833d150c7
Kenith Guanilo <kenithguanilo@gmail.com>
2025-10-24 11:20:55 -0500

    Add Footer component and update HomePageTemplate to include it

---
+++ src/molecules/footer/index.tsx
@@ -0,0 +1,14 @@
+
+export default function Footer() {
+  return (
+          <footer className="bg-(--simpsons-yellow) text-(--simpsons-text) text-center py-6 px-4 
+                         border-t-6 border-(--simpsons-border) shadow-inner">
+        <p className="font-simpsons text-lg mb-2">
+           Donutmind - Tu compañero para encontrar episodios de Los Simpsons
+        </p>
+        <p className="text-sm opacity-70">
+          DonutMind puede cometer errores. Siempre verifica la información importante.
+        </p>
+      </footer>
+  )
+}
--- src/templates/HomeTemplate/index.tsx
+++ src/templates/HomeTemplate/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Header from '../../organism/Header';
-import HeroSection from '../../organism/HeroSection';
-import CardsSection from '../../organism/CardSection';
-import ChatbotSection from '../../organism/ChatBotSection';
+import Header from '../../organism/header';
+import HeroSection from '../../organism/heroSection';
+import CardsSection from '../../organism/cardSection';
+import ChatbotSection from '../../organism/chatBotSection';
+import Footer from '../../molecules/footer';
 
 interface HomePageTemplateProps {
   siteTitle: string;
@@ -20,16 +21,9 @@ const HomePageTemplate: React.FC<HomePageTemplateProps> = ({
         <HeroSection />
         <CardsSection />
         <ChatbotSection />
+        <Footer />
       </main>
-      <footer className="bg-(--simpsons-yellow) text-(--simpsons-text) text-center py-6 px-4 
-                         border-t-6 border-(--simpsons-border) shadow-inner">
-        <p className="font-simpsons text-lg mb-2">
-           Donutmind - Tu compañero para encontrar episodios de Los Simpsons
-        </p>
-        <p className="text-sm opacity-70">
-          DonutMind puede cometer errores. Siempre verifica la información importante.
-        </p>
-      </footer>
+
     </div>
   );
 };
